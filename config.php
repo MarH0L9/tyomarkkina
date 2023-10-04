3e5a2f5969971b6ceb47701197a4c823ef35c78e
Marco <143179277+MarH0L9@users.noreply.github.com>
2023-10-04 17:51:45 +0300

    Update2config.php

--- config.php
+++ config.php
@@ -15,24 +15,17 @@ if ($conexionLocal) {
     $database = $local_credentials["database"];
 } else {
     // Intenta conectar a la base de datos de Azure
-    try {
+// Intenta conectar a la base de datos de Azure
         $server = getenv('DB_SERVER');
         $port = getenv('DB_PORT');
         $username = getenv('DB_USERNAME');
         $password = getenv('DB_PASSWORD');
         $database = getenv('DB_DATABASE'); 
-
-        $dsn = "mysql:host=$server;port=$port;dbname=$database";
-        $pdo = new PDO($dsn, $username, $password);
-        // Configurar el manejo de errores de PDO si es necesario
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        // Registrar el error en un archivo
-        file_put_contents("errors.txt", "Error de conexión a Azure: " . $e->getMessage() . "\n", FILE_APPEND);
-        
-        // Si la conexión a Azure falla, muestra un mensaje personalizado de error
-        die("Ha ocurrido un error en la conexión a la base de datos. Por favor, intenta de nuevo más tarde.");
-    }
+// Verifica la conexión
+if ($conn->connect_error) {
+    die("La conexión a la base de datos ha fallado: " . $conn->connect_error);
 }
 
+echo "Conexión a la base de datos exitosa.";
+$conn->close();
 ?>
