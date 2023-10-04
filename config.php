ea5d5ba97be345165ab592499ccdcfcfbb8ff8ff
Marco <143179277+MarH0L9@users.noreply.github.com>
2023-10-04 17:53:08 +0300

    Update3config.php

--- config.php
+++ config.php
@@ -1,19 +1,4 @@
 <?php
-
-// Define una variable para determinar si la conexión debe ser local o a Azure
-$conexionLocal = false;
-
-// Verifica si la conexión debe ser local (por ejemplo, si estás en un entorno de desarrollo)
-if ($conexionLocal) {
-    // Incluye el archivo de configuración local
-    include 'C:\xampp\htdocs\Projects\tunnukset.php';
-
-    // Utiliza las credenciales locales para la conexión a la base de datos
-    $server = $local_credentials["server"];
-    $username = $local_credentials["username"];
-    $password = $local_credentials["password"];
-    $database = $local_credentials["database"];
-} else {
     // Intenta conectar a la base de datos de Azure
 // Intenta conectar a la base de datos de Azure
         $server = getenv('DB_SERVER');
