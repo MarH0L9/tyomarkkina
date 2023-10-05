7dc22866f81610eee7eb3db7958d8228dbc896a1
Marco <143179277+MarH0L9@users.noreply.github.com>
2023-10-05 12:58:07 +0300

    Update functions.php

--- functions/functions.php
+++ functions/functions.php
@@ -1,35 +1,19 @@
 <?php
-function generateJobCard($jobData) {
-    $card = '<div class="col-md-12">';
-    $card .= '<a href="detalle_oferta.php?id=' . $jobData['ID'] . '" style="text-decoration: none; color: inherit;" target="_blank">'; // Enlace que envuelve toda la tarjeta
-    $card .= '<div class="card-body my-custom-card">';
-    
-    // Título en negrita sin márgenes y fondo lila claro
-    $card .= '<h3 class="card-title"><strong>' . $jobData['Otsikko'] . '</strong></h3>';
-    
-    // Fila para Sijainti, kunta a la izquierda, tyosuhde a la derecha
-    $card .= '<div class="row">';
-    $card .= '<div class="col-md-6">';
-    $card .= '<p><i class="fas fa-map-marker-alt" style="color: #0f0f10;"></i></i><strong> Sijainti:</strong> ' . $jobData['Sijainti'] . ', ' . $jobData['Kunta'] . '</p>';
-    $card .= '</div>';
-    $card .= '<div class="col-md-6">';
-    $card .= '<p><strong>Palvelusuhde:</strong> ' . $jobData['PalveluSuhde'] . '</p>';
-    $card .= '</div>';
-    $card .= '</div>';
-    
-    // Fila para Kuvaus
-    $card .= '<div class="row">';
-    $card .= '<div class="col-md-12">';
-    $card .= '<p><strong>Kuvaus:</strong> ' . $jobData['Kuvaus'] . '</p>';
-    $card .= '</div>';
-    $card .= '</div>';
-    
-    // Cierre de la tarjeta
-    $card .= '</div>';
-    $card .= '</a>'; // Cierra el enlace que envuelve toda la tarjeta
-    $card .= '</div>';
+function generateJobListItem($jobData) {
+    $item = '<li>';
+    $item .= '<h3>' . $jobData['Otsikko'] . '</h3>';
+    $item .= '<p><strong>Sijainti:</strong> ' . $jobData['Sijainti'] . ', ' . $jobData['Kunta'] . '</p>';
+    $item .= '<p><strong>Yrityksen Nimi:</strong> ' . $jobData['YrityksenNimi'] . '</p>';
+    $item .= '<p><strong>Julkaistu:</strong> ' . date('d.m.Y', strtotime($jobData['Julkaisupaiva'])) . '</p>';
+    $item .= '<p><strong>Kuvaus:</strong></p>';
+    $item .= '<p>' . nl2br($jobData['Kuvaus']) . '</p>';
+    $item .= '<p><strong>Vaatimukset:</strong></p>';
+    $item .= '<p>' . nl2br($jobData['Vaatimukset']) . '</p>';
+    if (!empty($jobData['YrityksenLinkki'])) {
+        $item .= '<p><strong>Yrityksen Linkki:</strong> <a href="' . $jobData['YrityksenLinkki'] . '" target="_blank">' . $jobData['YrityksenLinkki'] . '</a></p>';
+    }
+    $item .= '</li>';
 
-    return $card;
+    return $item;
 }
-
 ?>
