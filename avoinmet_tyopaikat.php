e0af8330cb7dc1f2a9d5ddc439aac9f975cd6781
Marco <143179277+MarH0L9@users.noreply.github.com>
2023-10-05 11:26:46 +0300

    Update avoinmet_tyopaikat.php

--- avoinmet_tyopaikat.php
+++ avoinmet_tyopaikat.php
@@ -95,8 +95,6 @@ if (isset($_GET['jobSearchText'])) {
     <script src="https://kit.fontawesome.com/07bb6b2702.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="scripts/haku_filter.js"></script>
     <script src="scripts/filtro.js"></script>
     <?php include 'config.php'; ?>
