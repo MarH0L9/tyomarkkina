780b99b51053dfe99c388bbe1f63d6f4195d2108
Marco <143179277+MarH0L9@users.noreply.github.com>
2023-10-10 14:19:52 +0300

    Update register.php

--- register.php
+++ register.php
@@ -50,9 +50,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-    <?php include 'header.php'; ?>
+    
 </head>
 <body>
+<?php include 'header.php'; ?>
 <div class="container regi" style="margin-top: 100px;">
     <fieldset>
         <legend>Rekisteröinti</legend>
