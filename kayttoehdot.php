64e281b6091f1a52debdd9ead86a2b37b1c41299
Marco <143179277+MarH0L9@users.noreply.github.com>
2023-10-11 14:49:09 +0300

    Add files via upload

---
+++ kayttoehdot.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="fi">
+<head>
+    <title>Sinun Työportaali</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/styles.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <?php include 'config.php';?>
+    <?php include 'functions/functions.php';?>
+</head>
+<body>
+<?php include 'header.php'; ?>
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div >
+            <h2>Käyttöehdot</h2>
+                
+                <div class="card-body">
+                    <p>Tämä on kuvitteellinen verkkosivusto, ja nämä käyttöehdot ovat demo-tarkoituksia varten.</p>
+
+                    <h4>Yleistä</h4>
+                    <p>Tämän verkkosivuston käyttö merkitsee näiden käyttöehtojen hyväksymistä.</p>
+
+                    <h4>Käyttäjän vastuut</h4>
+                    <p>Käyttäjä sitoutuu olemaan lähettämättä tai levittämättä sisältöä, joka on laitonta, haitallista, uhkaavaa, loukkaavaa tai joka loukkaa kolmannen osapuolen oikeuksia.</p>
+
+                    <h4>Immateriaalioikeudet</h4>
+                    <p>Kaikki sivuston sisältö on suojattu tekijänoikeudella ja muilla immateriaalioikeuksilla, ellei toisin mainita.</p>
+
+                    <h4>Muutokset käyttöehtoihin</h4>
+                    <p>Pidätämme oikeuden muuttaa näitä käyttöehtoja milloin tahansa. Muutokset tulevat voimaan heti, kun ne on julkaistu tällä sivustolla.</p>
+
+                    <h4>Rajoitettu vastuu</h4>
+                    <p>Tämän verkkosivuston sisältö tarjotaan "sellaisenaan". Emme takaa sivuston toimivuutta tai sisällön oikeellisuutta.</p>
+
+                    <h4>Yhteystiedot</h4>
+                    <p>Jos sinulla on kysyttävää näistä käyttöehdoista, voit ottaa meihin yhteyttä sähköpostitse osoitteeseen: esimerkki@email.fi.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+</div>
+<?php include 'footer.html'; ?>
+</body>
+</html>
---
+++ tietosuojaseloste.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="fi">
+<head>
+    <title>Sinun Työportaali</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/styles.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <?php include 'config.php';?>
+    <?php include 'functions/functions.php';?>
+</head>
+<body>
+<?php include 'header.php'; ?>
+<div class="container mt-5">
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header text-center">
+                    <h2>Tietosuojaseloste</h2>
+                </div>
+                <div class="card-body">
+                    <p>Tämä on kuvitteellinen verkkosivusto, ja tämä tietosuojaseloste on demo-tarkoituksia varten.</p>
+
+                    <h4>Kerätyt tiedot</h4>
+                    <p>Voimme kerätä ja käsitellä seuraavia tietoja: nimi, yhteystiedot ja muut perustiedot.</p>
+
+                    <h4>Tietojen käyttö</h4>
+                    <p>Käytämme kerättyjä tietoja palveluidemme tarjoamiseen, tilastollisiin tarkoituksiin ja muuhun asiakassuhteen hoitamiseen.</p>
+
+                    <h4>Tietojen jakaminen</h4>
+                    <p>Emme jaa henkilökohtaisia tietojasi kolmansille osapuolille ilman suostumustasi, paitsi lain sallimissa tai vaatimissa rajoissa.</p>
+
+                    <h4>Oikeutesi</h4>
+                    <p>Sinulla on oikeus tarkastella, korjata tai poistaa henkilökohtaisia tietojasi. Voit myös vastustaa tietojesi käsittelyä.</p>
+
+                    <h4>Yhteydenotto</h4>
+                    <p>Jos sinulla on kysyttävää tästä tietosuojaselosteesta tai henkilötietojesi käsittelystä, voit ottaa meihin yhteyttä sähköpostitse osoitteeseen: esimerkki@email.fi.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+</div>
+<?php include 'footer.html'; ?>
+</body>
+</html>
