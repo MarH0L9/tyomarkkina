9fe9a4f9681d9be0c7d5253cd5837b9ce5b1b1f7
Marco <143179277+MarH0L9@users.noreply.github.com>
2023-10-13 16:34:30 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,11 +1,12 @@
 <?php
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
-} 
+}
+
 require 'config.php'; 
 $title = 'Kirjaudu sisään';
 $css = 'css/styles.css';
-include "header.php";
+
 
 if (isset($_SESSION['user_id'])) {
     header("Location: index.php");
@@ -27,11 +28,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    if ($user && password_verify($Password, $user['pwd'])) { // Use password_verify() for password hashing
-        session_regenerate_id(true);  
-        $_SESSION['user_id'] = $user['ID'];
-        $_SESSION['login_success'] = true;
+    if ($user && password_verify($Password, $user['pwd'])) {
         
+        if($user['is_admin'] == 1) { /*tarkistaa onko admin*/
+            $_SESSION['is_admin'] = true;
+            header("Location: admin_panel.php");
+            exit();
+        } else {
+            $_SESSION['is_admin'] = false;
+        }
+        if($user['user_type'] == 'yritys') {
+            $businessQuery = "SELECT hyvaksytty FROM tyonantajat WHERE user_id = :user_id";
+            $businessStmt = $pdo->prepare($businessQuery);
+            $businessStmt->bindParam(':user_id', $user['id']);
+            $businessStmt->execute();
+            $businessUser = $businessStmt->fetch(PDO::FETCH_ASSOC);
+    
+            if($businessUser['hyvaksytty'] == 1) {
+                session_regenerate_id(true);  
+                $_SESSION['user_id'] = $user['id'];
+                $_SESSION['user_type'] = $user['user_type'];
+                $_SESSION['login_success'] = true;
+            } else {
+                $message = '<div class="alert alert-danger" role="alert"><i class="bi bi-exclamation-triangle"></i> Tiliäsi ei ole vielä hyväksytty. Ota yhteyttä tukeen.</div>';
+            }
+        } else {
+            session_regenerate_id(true);  
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['user_type'] = $user['user_type'];
+            $_SESSION['login_success'] = true;
+        }
     } else {
         $message = '<div class="alert alert-danger" role="alert"><i class="bi bi-exclamation-triangle"></i> Sähköposti tai salasana on virheellinen.</div>';
     }
@@ -48,20 +74,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <?php include "header.php"?>
 </head>
 <body>
 
-    <div class="container login" style="margin-top: 200px;">
-    <?php if (isset($_SESSION['login_success'])) {
-    echo '<div class="alert alert-success" role="alert"> Kirjautuminen onnistui. Sinut ohjataan pääsivulle 2 sekunnin kuluttua...</div>';
-    echo '<script>
-        setTimeout(function() {
-            window.location.href = "index.php";
-        }, 2000);
-    </script>';
-    unset($_SESSION['login_success']);  // Elimina la variable de sesión después de mostrar el mensaje
-}
-?>
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <?php if (isset($_SESSION['login_success'])) {
+                echo '<div class="alert alert-success" role="alert"> Kirjautuminen onnistui. Sinut ohjataan pääsivulle 2 sekunnin kuluttua...</div>';
+                echo '<script>
+                    setTimeout(function() {
+                        window.location.href = "index.php";
+                    }, 2000);
+                </script>';
+                unset($_SESSION['login_success']);  // Elimina la variable de sesión después de mostrar el mensaje
+                }
+            ?>
     <fieldset>
             <legend>Kirjaudu sisään </legend>
             <?php if (!empty($message)) : ?>
@@ -70,25 +99,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <form action="login.php" method="POST">
                 <!-- Email input -->
                 <div class="form-outline mb-4">
+                <label class="form-label" for="email">Sähköposti</label>
                     <input type="email" class="form-control dos" id="email" name="email" required>
-                    <label class="form-label" for="email">Sähköposti</label>
+                    
                 </div>
                 <!-- Password input -->
                 <div class="form-outline mb-4">
+                <label class="form-label" for="password">Salasana</label>
                     <input type="password" class="form-control" id="password" name="password" required>
-                    <label class="form-label" for="password">Salasana</label>
+                    
                 </div>
                 <!-- Submit button -->
+                <div class="text-center">
                 <button type="submit" class="btn btn-primary btn-block mb-4">Kirjaudu</button>
+                </div>
                 <div class="mb-3">
+                    <div class="text-center">
                     <a href="forgot.php" class="btn btn-link">Unohditko salasanan?</a>
+                </div>
                 </div>
             </form>
         </fieldset>
     </div>
-    
+    </div>
+    </div>
+    </div>
+    <?php include 'footer.html'; ?>
 
 </body>
 
-<?php include 'footer.html'; ?>
+
 </html>
