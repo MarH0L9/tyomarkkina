e939abfb4aa09cbba2e0b1a90fc11ac1ce0c1434
Marco <143179277+MarH0L9@users.noreply.github.com>
2023-10-17 11:22:21 +0300

    Update filtro.js

--- scripts/filtro.js
+++ scripts/filtro.js
@@ -7,7 +7,8 @@ function fetchJobs() {
     var tyonkieli = $("#tyonkieli").val();
     var tyoaika = $("#tyoaika").val();
     var tehtava = $("#tehtava").val();
-    var offersPerPage = $("#offersPerPage").val(); // Agregamos esta línea
+    
+    
 
     // Realizar solicitud Ajax para procesar la búsqueda y los filtros
     $.ajax({
@@ -21,32 +22,15 @@ function fetchJobs() {
             tyokieli: tyonkieli,
             tyoaika: tyoaika,
             tehtava: tehtava,
-            OffersPerPage: offersPerPage // Agregamos esta línea
+            
         },
         success: function (response) {
             $("#searchResults").html(response);
-        }
-    });
+    }
+});
 }
 
 $("#offersPerPage").change(fetchJobs); // Agregamos un listener para el desplegable
 
 $("#searchButton").on("click", fetchJobs);
 $("#sijainti, #julkaistu, #palvelusuhde, #tyonkieli, #tyoaika, #vaatimukset, #tehtava").on("input", fetchJobs);
-
-// Función para limpiar todos los filtros
-function clearFilters() {
-    $("#jobSearchText").val(''); // limpia el campo de búsqueda
-    $("#sijainti").val(''); // limpia el filtro sijainti
-    $("#julkaistu").val(''); // limpia el filtro julkaistu
-    $("#palvelusuhde").val(''); // limpia el filtro palvelusuhde
-    $("#tyonkieli").val(''); // limpia el filtro tyonkieli
-    $("#tyoaika").val(''); // limpia el filtro tyoaika
-    $("#tehtava").val(''); // limpia el filtro ala
-    // ... (Haz lo mismo para cualquier otro filtro que tengas)
-
-    fetchJobs(); // Vuelve a cargar todos los trabajos sin filtros
-}
-
-// Agrega un listener para el botón de limpiar filtros
-$("#clearFilters").on("click", clearFilters);
