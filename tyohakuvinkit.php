7cb865b4a2367aa561cb29404718422caf5fc794
Marco <143179277+MarH0L9@users.noreply.github.com>
2023-10-20 13:37:35 +0300

    Add files via upload

---
+++ tyohakuvinkit.php
@@ -0,0 +1,111 @@
+<?php if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}   
+?>
+
+
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+    <title>Avoinmet työpaikat</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <script src="https://kit.fontawesome.com/07bb6b2702.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js"></script>
+    <?php include 'config.php'; ?>
+</head>
+<body>
+<?php include 'header.php'; ?>
+
+<main>
+<div class="container mx-auto mt-5">
+    <div class="row justify-content-center mt-5">
+    <h1>Työnhakuvinkit - Noudata Parhaita Käytäntöjä Työpaikan Etsinnässä</h1>
+    <p>Tervetuloa Työnhakuvinkit-sivustolle! Olemme koonneet laajan valikoiman vinkkejä ja ohjeita, jotka auttavat sinua menestymään työpaikan etsinnässä Suomessa. Työnhaku voi olla haastavaa, mutta oikeiden tietojen ja strategioiden avulla voit lisätä mahdollisuuksiasi löytää unelmiesi työpaikka.</p>
+
+    <h2>Sisällysluettelo</h2>
+    <ul>
+        <li>Työnhakuprosessin Valmistelu</li>
+        <li>CV:n ja Ansioluettelon Luominen</li>
+        <li>Työhakemuksen Kirjoittaminen</li>
+        <li>Työhaastatteluun Valmistautuminen</li>
+        <li>Työnhakustrategiat</li>
+        <li>Verkostoituminen</li>
+        <li>Työnhaku Verkossa</li>
+        <li>Työnhakupalvelut</li>
+        <li>Työpaikan Etsintä Eri Aloilta</li>
+        <li>Teknologia- ja IT-ala</li>
+        <li>Terveydenhuoltoala</li>
+        <li>Koulutusala</li>
+        <li>Rakennusala</li>
+        <li>Matkailu- ja Ravintola-ala</li>
+        <li>Työpaikan Etsintä Eri Kaupungeissa</li>
+        <li>Helsinki</li>
+        <li>Tampere</li>
+        <li>Turku</li>
+        <li>Oulu</li>
+        <li>Jyväskylä</li>
+        <li>Kielitaito ja Työllistyminen Suomessa</li>
+        <li>Suomen Kielen Tärkeys</li>
+        <li>Kielikurssit</li>
+        <li>Työnhaku Ulkomailta Suomeen</li>
+        <li>Työluvan Hakeminen</li>
+        <li>Asunnon Etsiminen</li>
+        <li>Työnhakuvinkit Erityisryhmille</li>
+        <li>Opiskelijat ja Harjoittelijat</li>
+        <li>Maahanmuuttajat</li>
+        <li>Vammaiset Työnhakijat</li>
+        <li>Uusimmat Työmarkkinatrendit</li>
+        <li>Etätyömahdollisuudet</li>
+        <li>Digitalisaation Vaikutus Työpaikkoihin</li>
+        <li>Vihreät Työpaikat ja Kestävä Kehitys</li>
+        <li>Työnhakuun Liittyvät Resurssit</li>
+        <li>Hyödylliset Linkit</li>
+        <li>Kirjallisuussuositukset</li>
+        <li>Työnhakuun Liittyvät Webinaarit ja Seminaarit</li>
+        <li>Yhteystiedot ja Tuki</li>
+        <li>Ota Yhteyttä</li>
+        <li>Usein Kysytyt Kysymykset (UKK)</li>
+        <li>Työnhakuneuvonta</li>
+    </ul>
+
+    <h2>Työnhakuprosessin Valmistelu</h2>
+    <p>Kun aloitat työnhaun, yksi ensimmäisistä askelista on CV:n (Curriculum Vitae) ja ansioluettelon luominen. CV on dokumentti, joka esittelee koulutuksesi, työkokemuksesi ja muut tärkeät tiedot työnantajalle. Ansioluettelo puolestaan on tiivistelmä näistä tiedoista.</p>
+    <!-- Agrega más contenido específico aquí -->
+
+    <h2>CV:n ja Ansioluettelon Luominen</h2>
+    <p>Mitä CV:hen ja ansioluetteloon tulisi sisällyttää?</p>
+    <!-- Agrega más contenido específico aquí -->
+
+    <h2>Työhakemuksen Kirjoittaminen</h2>
+    <p>Työhakemus on ensimmäinen mahdollisuutesi tehdä vaikutus työnantajaan. Se tulisi räätälöidä kuhunkin työpaikkaan ja sisältää seuraavat kohdat:</p>
+    <!-- Agrega más contenido específico aquí -->
+
+    <h2>Työhaastatteluun Valmistautuminen</h2>
+    <p>Työhaastattelu on tärkeä vaihe työnhaussa. Valmistaudu huolellisesti:</p>
+    <!-- Agrega más contenido específico aquí -->
+
+    <h2>Työnhakustrategiat</h2>
+    <h3>Verkostoituminen</h3>
+    <p>Verkostoituminen on tärkeä osa työnhaussa. Tapaa alan ammattilaisia, osallistu tapahtumiin ja liity verkostoihin, jotka liittyvät kiinnostuksesi kohteisiin. Verkostosi voivat auttaa sinua löytämään piilotettuja työmahdollisuuksia.</p>
+    <!-- Agrega más contenido específico aquí -->
+
+    <h3>Työnhaku Verkossa</h3>
+    <p>Internet tarjoaa lukemattomia työnhakumahdollisuuksia. Käytä erilaisia työnhakupalveluita ja verkkosivustoja löytääksesi sopivia työpaikkoja.</p>
+    <!-- Agrega más contenido específico aquí -->
+
+    <h3>Työnhakupalvelut</h3>
+    <p>Tutustu erilaisiin työnhakupalveluihin, jotka voivat auttaa sinua löytämään työpaikkoja.</p>
+    <!-- Agrega más contenido específico aquí -->
+
+    <!-- Agrega más secciones y contenido según tus necesidades -->
+</div>
+</div>
+</main>
+<?php include 'footer.html'; ?>
+</body>
+</html>
