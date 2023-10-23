613a0b3aecb146965c2698817e8f7fe9ffdf1a5d
Marco <143179277+MarH0L9@users.noreply.github.com>
2023-10-23 13:44:34 +0300

    Update avoinmet_tyopaikat.php

--- avoinmet_tyopaikat.php
+++ avoinmet_tyopaikat.php
@@ -11,7 +11,7 @@ $searchTerm = isset($_GET['jobSearchText']) ? $_GET['jobSearchText'] : '';
 <!DOCTYPE html>
 <html lang="fi">
 <head>
-    <title>Avoinmet työpaikat</title>
+    <title>Avoimmet työpaikat</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="resources/images/logo/favicon.ico">
