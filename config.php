8b99918bed287c6ed0796973afe5eabb28f4f4d8
Marco <143179277+MarH0L9@users.noreply.github.com>
2024-04-11 15:32:18 +0300

    Update config.php

--- config.php
+++ config.php
@@ -8,22 +8,21 @@ $username = getenv('DB_USERNAME');
 $password = getenv('DB_PASSWORD');
 $database = getenv('DB_DATABASE');
 
-// Verifica si alguna de las variables de entorno está vacía
+
 if (empty($server) || empty($port) || empty($username) || empty($password) || empty($database)) {
     die("La configuración de la base de datos no está completa.");
 }
 
-// Intenta conectar a la base de datos de Azure
+//Omnian Azuren yhteys
 try {
     $dsn = "mysql:host=$server;port=$port;dbname=$database;charset=utf8mb4";
     $pdo = new PDO($dsn, $username, $password);
 
-    // Configurar el manejo de errores de PDO si es necesario
+    // Errors
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
- // Configurar la codificación de caracteres a utf8mb4
+ 
     $pdo->exec("set names utf8mb4");
 } catch (PDOException $e) {
-    // Manejo de errores
     echo "Error de conexión a Azure: " . $e->getMessage();
 }
 ?>
